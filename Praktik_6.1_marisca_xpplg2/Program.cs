f84913a2868eacb19232c128f3eb8ce63ce6ed2f
mariscafebyf <mariscafebyf@gmail.com>
2025-10-21 09:28:27 +0700

    Add files via upload

---
+++ Praktik_6.1_marisca_xpplg2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_6._1_marisca_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perulangan untuk menampilkan angka 1 sampai 10: ");
+
+            // Perulangan for dimulai di sini
+            for (int i = 1; i <= 10; i++)
+            {
+
+                // Blok kode yang akan diulang
+                Console.WriteLine("Angka ke-" + i);
+
+            }
+            Console.WriteLine("\nPerulangan selesai. ");
+
+
+
+
+
+
+        }
+    }
+}
