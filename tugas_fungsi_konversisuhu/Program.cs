a3f5fd69fdde20805a6ccaa3116897a0eb67bea1
mariscafebyf <mariscafebyf@gmail.com>
2025-10-21 14:24:07 +0700

    Add files via upload

---
+++ tugas_fungsi_konversisuhu/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_fungsi_konverensisuhu
+{
+    internal class Program
+    {
+        // Fungsi konversisuhu : mengubah suhu dari celcius  ke fahrenhit
+        static double konversisuhu(double c)
+        {
+            double f = (c * 9 / 5) + 32;
+            return f;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Program konversi suhu suhu celcius ke fahrenhit ===");
+
+            // Input dari pengguna
+            Console.Write("Masukkan suhu dalam celcius: ");
+            double suhuC = Convert.ToDouble(Console.ReadLine());
+
+            // Panggil fungsi konversisuhu
+            double suhuF = konversisuhu(suhuC);
+
+            // Tampilkan hasil konversi
+            Console.WriteLine($"{suhuC}°C sama dengan {suhuF}°F");
+
+            Console.WriteLine("=== program selesai ===");
+        
+
+
+
+
+
+        }
+    }
+}
---
+++ tugas_fungsi_rata rata/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_fungsi_rata_rata
+{
+    internal class Program
+    {
+        // Fungsi untuk menghitung rata-rata dari tiga nilai
+
+        static double HitungRataRata(double a, double b, double c)
+        {
+            return (a + b + c) / 3;
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Meminta input dari pengguna
+            Console.Write("Masukkan nilai pertama: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan nilai kedua: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ketiga: ");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+
+            // Memanggil fungsi dan menampilkan hasilnya
+            double hasil = HitungRataRata(n1, n2, n3);
+
+        }
+    }
+}
