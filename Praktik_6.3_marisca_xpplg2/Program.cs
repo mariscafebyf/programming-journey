54134c471e7c3db83db172f39ebdbdac2948e795
mariscafebyf <mariscafebyf@gmail.com>
2025-10-21 09:53:13 +0700

    Add files via upload

---
+++ Praktik_6.3_marisca_xpplg2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_6._3_marisca_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inisialisasi variabel untuk menyimpan input
+            string inputUser = "";
+
+            Console.WriteLine("Program akan terus berjalan hingga anda mengetik 'keluar' .");
+
+            // Perulangan selama isi variabel inputUser TIDAK SAMA DENGAN "keluar"
+            while (inputUser.ToLower() != "keluar")
+            {
+                Console.Write("\nKetik sesuatu (atau 'keluar' untuk berhenti): ");
+                inputUser = Console.ReadLine(); // Baca input dari user
+
+                Console.WriteLine("Anda mengetik: " + inputUser);
+            }
+                Console.WriteLine("\nProgram selesai. terima kasih!");
+        }
+    }
+}
---
+++ Praktik_6.5_marisca_xpplg2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_6._5_marisca_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Inisalisasi variabel penghitung (counter)
+            int angka = 1;
+
+            Console.WriteLine("Menampilkan angka 1 sampai 20:");
+
+            // 2. Blok 'do-while'
+            do
+            {
+                // Tampilakan nilai variabel 'angka' saat ini 
+                Console.WriteLine(angka);
+
+                // Tambahkan nilai 'angka' sebanyak 1 (increment)
+                angka++;
+
+                // Uji kondisi: Loop berlanjut selama 'angka' kurang dari atau sama dengan 
+            } while (angka <= 20);
+
+            Console.WriteLine("Selesai.");
+
+
+            
+        }
+    }
+}
